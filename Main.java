5a80a88017f4c4c688805004d5c028922497c6c7
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-11-17 19:49:31 +0000

    Create an array of int variables for the seats.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int [] seats = { 1 , 2, 3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15}; 
+// Intiatlize seat variables 1-15
+    String[] Students = new String [15]; 
+//String array of the names 
+    for (int i = 15 ; i < Students.length; i++) {
+      System.out.println("What is your name?" + seats); 
+
+      System.out.println(Students[i]); 
+// Create a for loop to ask for the names according to the seat number.  
+      stringScanner Students = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = Students.nextLine();
+
+     }
+    }
   }
-}
