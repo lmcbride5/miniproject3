9835b426cef1968d704c5035021d314951ee7aad
lmcbride5 <lilibeth.mcbride@patelhs.org>
2020-11-18 02:22:36 +0000

    Created a for loop to ask for the names.

--- Main.java
+++ Main.java
@@ -1,25 +1,27 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
-    Scanner scan = new Scanner(System.in); 
-  //Scanner to recieve user names 
+    Scanner scan = new Scanner(System.in);
+    // Scanner to recieve user names
 
-    int [] seats = { 1 , 2, 3 , 4 , 5 ,6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15}; 
-// Intiatlize seat variables 1-15
+    String[] Students = new String[15];
+    // String array of the names
 
-    int[] Students = new int [15]; 
-//String array of the names 
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    // Intiatlize seat variables 1-15
 
-    for (int i = 0 ; i < Students.length; i++) {
-      System.out.println("What is your name?" + seats); 
-
-      Students[i] = scan.nextInt(); 
+    for (int i = 0; i <= 14; i++) {
+      System.out.println("What is your students name?");
+      Students[i] = scan.next();
     }
-// Create a for loop to ask for the names according to the seat number with the scanner  
-    int count = 1; 
-    while (count <= 15) 
-      System.out.println(count); 
-  }
-}
+    // Create a for loop to ask for the names according to the seat number with the scanner 
+
+    int count = 0;
+
+    while (count <= 14) {
+      System.out.println(Students[count] + " is going to sit in #" + seats[count]);
+      count++;
+      // This does the final print of the user's names and seat number
